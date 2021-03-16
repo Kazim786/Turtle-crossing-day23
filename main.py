3937a352e806e248d77064f1f436b3672c817fda
Kazim Shabbir <Kazimshabbir2010@hotmail.com>
2021-03-16 17:26:47 -0500

    ive done it by myself

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.listen()
+
+
+player = Player()
+
+
+screen.onkey(player.up, "Up")
+
 
 game_is_on = True
 while game_is_on:
--- player.py
+++ player.py
@@ -1,7 +1,22 @@
+from turtle import Turtle, Screen 
+
+screen = Screen()
+screen.listen()
+
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
 FINISH_LINE_Y = 280
 
 
-class Player:
-    pass
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
+        
+    def up(self):
+        self.penup()
+        going_up = self.ycor() + MOVE_DISTANCE
+        self.goto(self.xcor(), going_up)
